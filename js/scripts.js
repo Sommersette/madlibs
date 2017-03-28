40d02803dee09610fc755ffef85e6fddf3e49131
Trevor Fast and Tyler Stephenson <trevor@>
2017-03-28 09:11:29 -0700

    refacot to for loop and remove redundancy.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
 $(function(){
   $("#formOne").submit(function(event){
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var userInput = ["person1", "person2", "animal", "exclamation", "verb","noun"];
+    for(i = 0; i <= userInput.length; i ++) {
+      var input = $("input#" + userInput[i]).val();
+      $("." + userInput[i]).append(input);
+    }
 
-
-
-
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".exclamation").append(exclamationInput);
-    $(".verb").append(verbInput);
-    $(".noun").append(nounInput);
-
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput = $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    //
+    //
+    //
+    //
+    // $(".person1").append(person1Input);
+    // $(".person2").append(person2Input);
+    // $(".animal").append(animalInput);
+    // $(".exclamation").append(exclamationInput);
+    // $(".verb").append(verbInput);
+    // $(".noun").append(nounInput);
+    //
     $("#story").show();
 
     event.preventDefault();
